d31de24128742bf6683f73f4739639522e613a0e
madcow95 <lobasketve@naver.com>
2021-02-02 22:07:03 +0900

    AtHome

---
+++ AtHome/src/com/hello/callor/Home.java
@@ -0,0 +1,25 @@
+package com.hello.callor;
+
+public class Home {
+	
+	public static void main(String args[]) {
+		
+		System.out.println(30 + 40);
+		System.out.println(30 * 40);
+		System.out.println();
+		
+		System.out.print("1995 ");
+		System.out.print("0313");
+		System.out.println();
+		
+		int num1 = 13;
+		int num2 = 12;
+		int num3 = 11;
+		
+		System.out.println(num1 + num2);
+		System.out.println(num1 - num3);
+		System.out.println(num1 / num2);
+		System.out.println(num1 % num3);
+	}
+
+}
