9f23e4f3804ca1e8a20976641543a601babcee0b
madcow95 <lobasketve@naver.com>
2021-02-02 14:36:14 +0900

    명령어

---
+++ Hello/src/Hello.java
@@ -0,0 +1,16 @@
+
+public class Hello {
+	
+	public static void main(String args[]) {
+		System.out.println("안녕하세요");
+		System.out.println(30 + 40);
+		System.out.println(30 * 40);
+		System.out.println(30 / 40);
+		System.out.println(30 - 40);
+		System.out.println("이름 : 홍길동");
+		System.out.println("주소 : 서울시");
+		System.out.println("전화 : 010-111-1111");
+	
+	}
+	
+}
