0ef6bb49ebdd90d7b2f6d3cf04840a9f9acceceb
madcow95 <lobasketve@naver.com>
2021-02-07 19:31:33 +0900

    home

--- Java_007/src/com/callor/practice/practice_02.java
+++ Java_007/src/com/callor/practice/practice_02.java
@@ -5,14 +5,14 @@ public class practice_02 {
 	public static void main(String args[]) {
 		
 		int pay = 51_892;
-		int paper = 10000;		
+		int paper = 10_000;		
 		int count = 0;
 		int i = 0;
 				
 		while(pay > 0) {
 			
 			count = pay / paper;
-			System.out.println(paper + "원권은" + count + "장.");
+			System.out.println(paper + "원권은" + count + "장");
 			pay -= (count * paper);
 			
 			if(i % 2 == 0) {
