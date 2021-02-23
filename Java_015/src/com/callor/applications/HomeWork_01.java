42a7d69e4a60d6eaf4d31bb8c59d7f1e1cfe1be4
madcow95 <lobasketve@naver.com>
2021-02-23 16:43:07 +0900

    homework

---
+++ Java_015/src/com/callor/applications/HomeWork_01.java
@@ -0,0 +1,44 @@
+package com.callor.applications;
+
+import java.util.Random;
+
+public class HomeWork_01 {
+
+	public static void main(String[] args) {
+
+		Random rnd = new Random();
+		PrimeServiceV2 psV2 = new PrimeServiceV2();
+		// 정수 100개를 저장할 배열 생성
+		int[] rndNum = new int[100];
+		// 임의의 정수 100개를 저장할 배열 생성
+		int[] intPrime = new int[rndNum.length];
+		// 1 ~ 100 의 범위 중 임의의 정수 100 개를 rndNum[0 ~ 99] 배열에 저장
+		for (int i = 0; i < rndNum.length; i++) {
+
+			rndNum[i] = rnd.nextInt(100) + 1;
+		}
+
+		for (int i = 0; i < rndNum.length; i++) {
+			// PrimeServiceV2에 rndNum[0 ~ 99]의 배열에 저장된 값을 보내 계산하게하고
+			// return에 의해 되돌아온 값을 intPrime[0 ~ 99] 배열에 저장
+			intPrime[i] = psV2.prime(rndNum[i]);
+		}
+		
+		int intSum = 0;		
+		for (int i = 0; i < rndNum.length; i++) {
+			// intPrime[0 ~ 99]에 저장된 임의의 변수 100 개를 intSum에 저장하고 덧셈
+			intSum += intPrime[i];
+		}
+		System.out.println("임의의 정수 100개 중 소수");
+		
+		for (int i = 0; i < rndNum.length; i++) {
+			if (intPrime[i] > 0) {
+
+				System.out.print(intPrime[i] + ", ");
+			}
+		}
+		System.out.println();
+		System.out.println("소수인 수들의 합 : " + intSum);
+	}
+
+}
