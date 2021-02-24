4099555084174a52eeb58131f19affc488a661b1
madcow95 <lobasketve@naver.com>
2021-02-24 21:41:20 +0900

    student

--- Java_016/src/com/callor/score/Score_06.java
+++ Java_016/src/com/callor/score/Score_06.java
@@ -20,15 +20,15 @@ public class Score_06 {
 			scoreVO[i].intEng = rnd.nextInt(100) + 1;
 			scoreVO[i].intMath = rnd.nextInt(100) + 1;
 		}
-																																				// 다시 해봐라 이거
+		
 		System.out.println("========================================");
 		System.out.println("국어\t영어\t수학\t합계\t평균");
 		System.out.println("----------------------------------------");
 		for(int i = 0; i < scoreVO.length; i++) {
 			
 			int sum = scoreVO[i].intKor;
-			sum = scoreVO[i].intEng;
-			sum = scoreVO[i].intMath;
+			sum += scoreVO[i].intEng;
+			sum += scoreVO[i].intMath;
 			
 			float floatAvg = (float)sum / 3;
 			System.out.printf("%d\t%d\t%d\t%d\t%3.2f\n",scoreVO[i].intKor,scoreVO[i].intEng,scoreVO[i].intMath,sum,floatAvg);
