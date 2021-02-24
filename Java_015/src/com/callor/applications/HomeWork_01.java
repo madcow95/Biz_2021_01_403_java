df9600372bdd66a61bbfcb3e4523b6c18d95f21b
madcow95 <lobasketve@naver.com>
2021-02-24 09:34:09 +0900

    homework

--- Java_015/src/com/callor/applications/HomeWork_01.java
+++ Java_015/src/com/callor/applications/HomeWork_01.java
@@ -12,19 +12,21 @@ public class HomeWork_01 {
 		int[] intPrime = new int[rndNum.length];
 
 		for (int i = 0; i < rndNum.length; i++) {
-
+			// rndNum[0 ~ 99] 배열에 2 ~ 101 중 임의의 정수 저장
 			rndNum[i] = rnd.nextInt(100) + 2;
 		}
-
+		
 		for (int i = 0; i < rndNum.length; i++) {
+			// prime() method에 rndNum[i] 배열 전달 후 결과값 intPrime[i]배열에 저장
 			intPrime[i] = psV2.prime(rndNum[i]);
 		}
 		
 		int intSum = 0;
 		for (int i = 0; i < rndNum.length; i++) {
 			if (intPrime[i] == -1) {
-
+				// -1 값이 return되면 뭘시키지;
 			} else {
+				// -1이 아닌 값이 return 되면 임의의 정수 중 소수들 intSum에 더하여 저장
 				intSum += intPrime[i];
 			}
 		}
@@ -32,8 +34,9 @@ public class HomeWork_01 {
 		System.out.println("임의의 정수 " + rndNum.length + "개 중 소수");
 		for (int i = 0; i < rndNum.length; i++) {
 			if (intPrime[i] == -1) {
-
+				// ?
 			} else {
+				// -1이 아닌 값이 return 되면 임의의 정수 중 소수들만 Console에 출력
 				System.out.print(intPrime[i] + ", ");
 			}
 		}
