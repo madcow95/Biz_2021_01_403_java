e9424aa7fd203b5caedb72b3cf276d02b01ae6e8
madcow95 <lobasketve@naver.com>
2021-03-03 16:33:42 +0900

    cart

--- Java_021_ShopManager/src/com/callor/shop/CartMenuV1.java
+++ Java_021_ShopManager/src/com/callor/shop/CartMenuV1.java
@@ -31,18 +31,18 @@ public class CartMenuV1 implements CartMenu {
 			if (select.equals("QUIT") || select.equals("q") || select.equals("Q")) {
 				return null;
 			}
-			int intSelect = Integer.valueOf(select);
+			int intSelect = Integer.valueOf(select); // 선생님 : 여기서 exception 뜰텐데 작동이 잘 되나 물어보심 / 여기서 try문 사용해서 exception여지 안준다
 			// 1 <= intSelect <= 3 일때만 실행
-			if (intSelect >= 1 && intSelect <= 3) {
+			if (intSelect >= 1 && intSelect <= 3) { // 1. 여기를 없애고
 				if (intSelect == 1) {
 					csV1.inputCart();
 				} else if (intSelect == 2) {
 					csV1.printCart();
 				} else if (intSelect == 3) {
-					csV1.printUser();
+					csV1.printUser(); // 2. 이 세개만 남겨놓고
 				}
 				// 그 외에는 아래 실행
-			} else {
+			} else { // 3. 여기만 남겨 놓으면 되지 않을까?
 				System.out.println("1 ~ 3 중에서만 입력하세요");
 			}
 		}
